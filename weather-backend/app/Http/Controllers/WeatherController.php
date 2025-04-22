ceab306f28d878d7de5d72c789effb7153b967c2
BrianSechelo <sechbrian123.gmail.com>
2025-04-22 15:32:12 -0700

    feat(api): extend weather endpoint to include 3-day forecast

--- weather-backend/app/Http/Controllers/WeatherController.php
+++ weather-backend/app/Http/Controllers/WeatherController.php
@@ -3,6 +3,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Http;
+use Illuminate\Support\Carbon;
 
 class WeatherController extends Controller
 {
@@ -22,15 +23,49 @@ class WeatherController extends Controller
         $lat = $geoResponse[0]['lat'];
         $lon = $geoResponse[0]['lon'];
 
+        $unit = $request->query('unit', 'metric');  
+
         $weatherResponse = Http::get('https://api.openweathermap.org/data/2.5/weather', [
             'lat' => $lat,
             'lon' => $lon,
             'appid' => config('services.openweather.key'),
-            'units' => 'metric', // or 'imperial'
+            'units' => $unit, // or 'imperial'
         ]);
         if ($weatherResponse->failed()) {
             return response()->json(['message' => 'Unable to fetch weather'], 500);
         }
+
+        $forecastResponse = Http::get('https://api.openweathermap.org/data/2.5/forecast', [
+            'lat' => $lat,
+            'lon' => $lon,
+            'appid' => config('services.openweather.key'),
+            'units' => $unit,
+        ]);
+
+        if ($forecastResponse->failed()) {
+            return response()->json(['message' => 'Unable to fetch forecast'], 500);
+        }
+
+        $forecastList = $forecastResponse->json()['list'];
+        $grouped = [];
+
+        foreach ($forecastList as $entry) {
+            $date = Carbon::parse($entry['dt_txt'])->format('Y-m-d');
+            $hour = Carbon::parse($entry['dt_txt'])->format('H');
+
+            // Pick midday (12:00) forecast to represent the day
+            if ($hour === '12') {
+                $grouped[$date] = [
+                    'date' => Carbon::parse($entry['dt_txt'])->format('l, M j'),
+                    'temp' => $entry['main']['temp'],
+                    'description' => $entry['weather'][0]['description'],
+                    'icon' => $entry['weather'][0]['icon'],
+                ];
+            }
+        }
+
+        // Return only next 3 days
+        $forecast = array_values(array_slice($grouped, 0, 3));
 
         return response()->json([
             'location' => $city,
@@ -38,6 +73,8 @@ class WeatherController extends Controller
             'description' => $weatherResponse['weather'][0]['description'],
             'humidity' => $weatherResponse['main']['humidity'],
             'wind_speed' => $weatherResponse['wind']['speed'],
+            'icon' => $weatherResponse['weather'][0]['icon'],
+            'forecast' => $forecast
         ]);
     }
 }
